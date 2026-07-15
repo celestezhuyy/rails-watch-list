201e1dac7eee5336089de55a771b838d0cfaa570
Celeste Zhu <celestezhuyy@gmail.com>
2026-07-15 16:17:52 +0900

    added validation to prevent adding the same movie multiple times

--- app/models/list.rb
+++ app/models/list.rb
@@ -7,4 +7,26 @@ class List < ApplicationRecord
 
   validates :name, uniqueness: true, presence: true
   validates_associated :bookmarks
+  validate :unique_movies_in_bookmarks
+
+  private
+
+  def unique_movies_in_bookmarks
+    active_bookmarks = bookmarks.reject(&:marked_for_destruction?)
+    seen_movie_ids = []
+    has_duplicates = false
+
+    active_bookmarks.each do |bookmark|
+      next if bookmark.movie_id.blank?
+      if seen_movie_ids.include?(bookmark.movie_id)
+        bookmark.errors.add(:movie_id, "has already been selected in this list")
+        has_duplicates = true
+      else
+        seen_movie_ids << bookmark.movie_id
+      end
+    end
+    if has_duplicates
+      errors.add(:base, "You cannot add the same movie multiple times.")
+    end
+  end
 end
